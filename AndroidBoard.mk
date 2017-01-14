9b2b183d26ad45a1792634ddc122ab9ff23eb54b
MALCOLM MEDINA RIASCOS <ASAZING@users.noreply.github.com>
2017-01-14 13:47:08 -0500

    Move to AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,3 +4,9 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
+
+# Create a link for the WCNSS config file, which ends up as a writable
+# version in /data/misc/wifi
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
+    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
