331e4424e22bdbf64a7c7c6889fe889dbc7c6f8b
MALCOLM MEDINA RIASCOS <ASAZING@users.noreply.github.com>
2017-01-14 13:47:44 -0500

    Delete Wlan

--- Android.mk
+++ Android.mk
@@ -21,10 +21,4 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-    ln -sf /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
-    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
-
 endif
