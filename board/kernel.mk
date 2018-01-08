97671eee9989c103f80bc0d9375e46c0fcb4dfc5
Efran <efren.saenz25@gmail.com>
2018-01-08 12:57:43 -0500

    Update
    fixed

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # kernel
-
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/wiko/msm8916
 TARGET_KERNEL_CONFIG := lineage_l5510_defconfig
 
-BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 androidboot.selinux=permissive phy-msm-usb.floated_charger_enable=1
+# CMDLINE
+BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
